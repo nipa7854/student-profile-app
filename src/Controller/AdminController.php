060f54fbf0a67d4b374c0f4fb181345e15db18d0
nipa7854 <nipa7854@gmail.com>
2025-04-21 19:13:30 +0600

    Admin_Role

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Admin;
 use App\Entity\Student;
 use App\Form\AdminType;
+use App\Form\StudentType;
 use App\Repository\StudentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -49,5 +50,40 @@ class AdminController extends AbstractController
         return $this->render('admin/dashboard.html.twig', [
             'students' => $students,
         ]);
+    }
+
+    #[Route('/admin/student/{id}/edit', name: 'admin_student_edit')]
+    public function editStudent(int $id, Request $request, EntityManagerInterface $em): Response
+    {
+        $student = $em->getRepository(Student::class)->find($id);
+        if (!$student) {
+            throw $this->createNotFoundException('Student not found');
+        }
+
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            $this->addFlash('success', 'Student updated successfully!');
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
+        return $this->render('admin/edit_student.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/student/{id}/delete', name: 'admin_student_delete')]
+    public function deleteStudent(int $id, EntityManagerInterface $em): Response
+    {
+        $student = $em->getRepository(Student::class)->find($id);
+        if ($student) {
+            $em->remove($student);
+            $em->flush();
+            $this->addFlash('success', 'Student deleted successfully!');
+        }
+
+        return $this->redirectToRoute('admin_dashboard');
     }
 }
